6b9978035847de60b6e506948c2c2652fd6745f4
Sam Hodak <slhodak@gmail.com>
2019-05-16 17:33:59 -0700

    Stub api routes

--- server/index.js
+++ server/index.js
@@ -7,3 +7,11 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 app.listen(3010, () => { 
   console.log('Reviews module server listening on port 3010!'); 
 });
+
+app.get('/id/impression', (req, res) => {
+  // respond with relevant data from restaurants table based on restaurant's id
+});
+
+app.get('/id/reviews', (req, res) => {
+  // respond with all reviews for this restaurant's id
+});
