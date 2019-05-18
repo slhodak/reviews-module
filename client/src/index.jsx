21c85ad76ea6be43e0fb9ccdc09c9e49c4efb090
Sam Hodak <slhodak@gmail.com>
2019-05-18 15:53:41 -0700

    Add styles file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReviewsModule from './components/reviewsModule.jsx';
+import './styles/styles.css';
 
 ReactDOM.render(<ReviewsModule />, document.getElementById('reviews'));
