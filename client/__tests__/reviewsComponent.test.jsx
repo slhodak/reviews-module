d3fd0ea743f8c47137f0c36048d5018bd160234f
Sam Hodak <slhodak@gmail.com>
2019-05-20 16:24:27 -0700

    Add a summary component snapshot test

--- client/__tests__/reviewsComponent.test.jsx
+++ client/__tests__/reviewsComponent.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Reviews from '../src/components/reviewsComponent.jsx';
 
-it('renders correctly', () => {
+it('renders the reviews top-level component', () => {
   const tree = renderer
     .create(<Reviews restaurantId={1} />)
     .toJSON();
