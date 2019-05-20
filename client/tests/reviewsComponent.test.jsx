25b396c6c5582ff49f9d1036e11ab2070fdb8b03
Sam Hodak <slhodak@gmail.com>
2019-05-20 12:18:47 -0700

    run jest init

--- client/tests/reviewsComponent.test.jsx
+++ client/tests/reviewsComponent.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Reviews from '../client/src/components/reviewsComponent.jsx';
+import Reviews from '../src/components/reviewsComponent.jsx';
 
 it('renders correctly', () => {
   const tree = renderer
