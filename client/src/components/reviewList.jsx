6347bc3be171f0bd9545d9a5fbb601809cbc9d2c
Sam Hodak <slhodak@gmail.com>
2019-05-21 13:36:52 -0700

    Create ReviewList which maps reviews from props

--- client/src/components/reviewList.jsx
+++ client/src/components/reviewList.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Review from './review.jsx';
 
-export default () => (
-  <div>
-    <h1>reviews render here</h1>
-  </div>
-);
+function ReviewList(props) {
+  const { reviews } = props;
+  return (
+    <div>
+      <h1>Reviews...</h1>
+      {reviews.map(review => (
+        <Review review={review} />
+      ))}
+    </div>
+  );
+}
+
+ReviewList.propTypes = {
+  reviews: PropTypes.array.isRequired
+};
+
+export default ReviewList;
