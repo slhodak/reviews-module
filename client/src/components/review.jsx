bdd1056ffecd92bd0f103288aba7c63d6543bb18
Sam Hodak <slhodak@gmail.com>
2019-05-21 15:20:39 -0700

    Layout & style user area of review

--- client/src/components/review.jsx
+++ client/src/components/review.jsx
@@ -4,8 +4,18 @@ import PropTypes from 'prop-types';
 function Review(props) {
   const { review } = props;
   return (
-    <div>
-      <h3>{review.diner}</h3>
+    <div id="single-review">
+      <div id="reviewer">
+        <div id="initials-icon">
+          <div>J</div>
+        </div>
+        <p id="name">Jaclyn</p>
+        <p id="city">Baltimore</p>
+        <p id="reviews-count"><span id="review-icon" />2 reviews</p>
+      </div>
+      <div id="review-details">
+
+      </div>
     </div>
   );
 }
