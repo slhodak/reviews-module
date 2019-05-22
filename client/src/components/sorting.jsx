eb5ef547fe4459912015f16ba910cc8ab55541d1
Sam Hodak <slhodak@gmail.com>
2019-05-21 19:52:36 -0700

    Layout sorting area

--- client/src/components/sorting.jsx
+++ client/src/components/sorting.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 export default () => (
-  <div>
-    <h3>Sort by</h3>
+  <div className="sorting-panel">
+    <h4>Sort by</h4>
+    <select className="sorter" />
+    <h4>Filters</h4>
+    <div className="filters">
+      <div clasName="filter-button">
+        <input type="checkbox" className="filter-checkbox" />
+        <span className="filter-name">Good for groups</span>
+      </div>
+    </div>
   </div>
 );
