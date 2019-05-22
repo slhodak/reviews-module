b0d209cfc47f4b7e4222c3a5f48fdbb96c373027
Sam Hodak <slhodak@gmail.com>
2019-05-21 20:12:39 -0700

    style sorting area

--- client/src/components/sorting.jsx
+++ client/src/components/sorting.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 export default () => (
   <div className="sorting-panel">
     <h4>Sort by</h4>
-    <select className="sorter" />
+    <div className="sort-area">
+      <div className="sort-dropdown">
+        <span className="sorter-name">Newest</span>
+        <i className="sorter-icon" />
+      </div>
+    </div>
     <h4>Filters</h4>
     <div className="filters">
-      <div clasName="filter-button">
+      <div className="filter-button">
         <input type="checkbox" className="filter-checkbox" />
         <span className="filter-name">Good for groups</span>
       </div>
