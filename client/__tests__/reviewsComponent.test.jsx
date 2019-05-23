4175246a42758e39938ab8526b432baa800a56f7
Sam Hodak <slhodak@gmail.com>
2019-05-22 22:09:17 -0700

    Added successful test to sort options list

--- client/__tests__/reviewsComponent.test.jsx
+++ client/__tests__/reviewsComponent.test.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Reviews from '../src/components/reviewsComponent.jsx';
 
-it('renders the reviews top-level component', () => {
-  const tree = renderer
-    .create(<Reviews restaurantId={1} />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Reviews Component', () => {
+  it('renders the reviews component and its children', () => {
+    const tree = renderer
+      .create(<Reviews restaurantId={1} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
