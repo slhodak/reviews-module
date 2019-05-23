a5a923e9d94189380a876ed0f69499ba416b8a28
Sam Hodak <slhodak@gmail.com>
2019-05-22 18:41:28 -0700

    Remove url loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,13 +17,6 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.svg/,
-        use: {
-          loader: 'svg-url-loader',
-          options: {}
-        }
       }
     ]
   }
