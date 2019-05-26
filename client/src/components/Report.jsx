c0d715d4ed7812926b324f914e94d5c3e0ded89e
Sam Hodak <slhodak@gmail.com>
2019-05-26 12:51:24 -0700

    Create functional stateless Report component

--- client/src/components/Report.jsx
+++ client/src/components/Report.jsx
@@ -1,2 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+const Report = (props) => {
+
+  return (
+    <div className="report-popup">
+      <h3>Report this review as inappropriate?</h3>
+      <p>If you believe this review should be removed from FreeSeats, please let us know and someone will investigate.</p>
+      <textarea />
+      <div className="report-buttons">
+        <button type="submit" className="report">Report</button>
+        <button type="button" className="cancel">Cancel</button>
+      </div>
+    </div>
+  );
+};
+
+export default Report;
