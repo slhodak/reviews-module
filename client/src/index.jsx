17498927664ade840d35696f875f9b4125bcf799
Sam Hodak <slhodak@gmail.com>
2019-05-28 18:01:26 -0700

    Alter DOM target

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,5 +4,5 @@ import Reviews from './components/ReviewsComponent.jsx';
 
 // How is id sent to module? Middleware for express to parse param?
 ReactDOM.render(
-  <Reviews restaurantId={3} />, document.getElementById('main')
+  <Reviews restaurantId={3} />, document.getElementById('reviews')
 );
